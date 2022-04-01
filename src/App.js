a8b9469a8e60deb913dfcb6e5ca4269206e5144d
Jlferrete <joseluisferrete@gmail.com>
2022-04-01 19:41:04 +0200

    Configurando SASS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Button } from "semantic-ui-react"
 
 export default function App() {
   return (
-    <div>
-      <h1>Hola Mundo!</h1>
-      <Button primary>Click Me</Button>
+    <div className='app'>
+      <h1 className='app__title'>Hola Mundo!</h1>
     </div>
   );
 }
