75cceffb677c7c4273cab83f22cef36357265168
jlferrete <joseluisferrete@gmail.com>
2022-04-03 00:06:07 +0200

    Estructura del login admin

--- src/layouts/AdminLayout/AdminLayout.js
+++ src/layouts/AdminLayout/AdminLayout.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { LoginAdmin } from "../../pages/Admin"
 import "./AdminLayout.scss";
 
 export function AdminLayout(props) {
-    const { children } = props;
-    
-    return (
+  const { children } = props;
+  const auth = null;
+
+  if (!auth) return <LoginAdmin />
+
+  return (
     <div>
-        <p>AdminLayout</p>
-        
-        { children }    
+      <p>AdminLayout</p>
+
+      {children}
     </div>
   )
 }
