3e9bac8491d6eb97a8dde43ec1be35fff02dd6e2
jlferrete <joseluisferrete@gmail.com>
2022-04-12 23:49:33 +0200

    Añadiendo lógica el TopMenu

--- src/components/Admin/TopMenu/TopMenu.js
+++ src/components/Admin/TopMenu/TopMenu.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { Icon, Menu } from "semantic-ui-react";
+import { useAuth } from "../../../hooks";
 import "./TopMenu.scss";
 
 export function TopMenu() {
 
+    const { auth, logout } = useAuth();
+    const renderName = () => {
+        if (auth.me?.first_name && auth.me?.last_name) {
+            return `${auth.me.first_name} ${auth.me.last_name}`;
+        }
+        return auth.me?.email;
+    };
 
     return (
         <Menu fixed="top" className="top-menu-admin">
@@ -12,8 +20,8 @@ export function TopMenu() {
             </Menu.Item>
 
             <Menu.Menu position="right">
-                <Menu.Item>Hola, usuario...</Menu.Item>
-                <Menu.Item>
+                <Menu.Item>Hola, {renderName()}</Menu.Item>
+                <Menu.Item onClick={logout}>
                     <Icon name="sign-out" />
                 </Menu.Item>
             </Menu.Menu>
